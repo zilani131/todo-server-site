f65e549f77412a31f589dc16552284b86660ac05
zilani131 <zilani.cuet@gmail.com>
2022-07-01 11:14:58 +0600

    My first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
