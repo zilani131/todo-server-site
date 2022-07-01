1a00209da1126daf3789546c437aae54847b9398
zilani131 <zilani.cuet@gmail.com>
2022-07-01 16:40:20 +0600

    database connected

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+// pw: 9fYXJTouapDtgZzd
+// user: todolist
+const uri = "mongodb+srv://todolist:9fYXJTouapDtgZzd@cluster0.jeq1a.mongodb.net/?retryWrites=true&w=majority";
+console.log("zilani");
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+
+});
+async function run() {
+  try {
+    await client.connect();
+    const itemsCollection = client.db("Todolist").collection("sample1");
+    app.get("/home", async (req, res) => {
+      const query = {};
+      const cursor = itemsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+      console.log(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
   })
